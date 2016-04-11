9f096b9a22f8bfb9c79c61df69e8d88e593df6a2
Adam Bæhrenz Björgvinsson <adambjorgvinsson@gmail.com>
2016-04-11 21:07:05 +0000

    listing concerts

--- script/js.js
+++ script/js.js
@@ -1,11 +1,38 @@
 
+function imgError(el){
+    $(el).attr("src","http://moredisciples.com/wp-content/uploads/2016/01/concert-silhouette-200.jpg")
+}
 
-$.ajax({
-    'url': 'http://apis.is/concerts',
-    'type': 'GET',
-    'dataType': 'json',
-    'success': function(response) {
+$(document).ready(function(){
 
-    }
+    $(document).on('mouseover','li' ,function(e){
+        $(e.currentTarget).find('p').show();
+        $(e.currentTarget).find('.overlay').show();
 
-});
+    });
+    $(document).on('mouseleave','li' ,function(e){
+        $(e.currentTarget).find('p').hide();
+        $(e.currentTarget).find('.overlay').hide();
+    });
+
+    $.ajax({
+        'url': 'https://apis.is/concerts',
+        'type': 'GET',
+        'dataType': 'json',
+        'success': function(response) {
+            var ul = $('#data')
+            var array = response.results
+            for(var i=0;i<array.length;i++){
+                var overlay = $('<div class="overlay" ></div>')
+                var img = $('<img src="'+ array[i].imageSource +'" onerror="imgError(this)"/>')
+                var p = $('<p>'+array[i].name +'</p>')
+                var li = $('<li></li>')
+
+                p.appendTo(li)
+                img.appendTo(li)
+                overlay.appendTo(li)
+                li.appendTo(ul)
+            }
+        }
+    });
+})
